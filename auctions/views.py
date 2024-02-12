b7f112f718a550ce59408609c73400b3ce701d61
Sam Ordonez <samsonordonez.ce@gmail.com>
2024-02-11 21:26:19 -0800

    "added the active listing functionality, create new listing functionality"

--- auctions/views.py
+++ auctions/views.py
@@ -4,11 +4,54 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Category, Listing
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    active_listings = Listing.objects.filter(is_active = True)
+    return render(request, "auctions/index.html", {
+        "listings": active_listings
+    })
+
+def create_listing(request):
+    if request.method == "GET":
+        all_categories = Category.objects.all().order_by('category')
+        return render(request, "auctions/create.html", {
+            "categories": all_categories
+        })
+    
+    #Meaning, not "GET" but "POST"
+    else:
+
+        #Get the data from form
+        title           = request.POST["title"]
+        description     = request.POST["description"]
+        image_url       = request.POST["image_url"]
+        price           = request.POST["price"]
+        category        = request.POST["category"]
+
+        #Verify the user
+        current_user = request.user
+
+        #Get all content about the particular category
+        category_data = Category.objects.get(category_name = category)
+
+        #Create new listing object
+        new_listing = Listing(
+            title           = title,
+            description     = description,
+            image_url       = image_url,
+            price           = float(price),
+            category        = category_data,
+            owner           = current_user,
+
+        )
+
+        #Insert new listing object into django database
+        new_listing.save()
+
+        #redirect into index page
+        return HttpResponseRedirect(reverse(index))
 
 
 def login_view(request):
